2b776442549f0cbfdcea89bf84d5d65ea64799f2
Pera Knezevic <predragknezevic@icloud.com>
2023-11-20 10:24:53 +0100

    tailwind typography, font change, dummy content

---
+++ app/tekstovi/[slug]/page.tsx
@@ -0,0 +1,33 @@
+import React from "react"
+
+const Tekst = ({ params }: { params: { slug: string } }) => {
+  return (
+    <article className="prose lg:prose-xl mx-auto my-10">
+      <h1 className="text-emerald-900 text-4xl">
+        Lorem, ipsum dolor sit amet consectetur adipisicing.
+      </h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet earum
+        obcaecati maiores nisi modi nulla aut asperiores praesentium. Hic
+        delectus corrupti nisi! Porro iusto eveniet nisi incidunt numquam? Autem
+        deserunt eligendi voluptas reprehenderit sequi repellendus error
+        accusamus obcaecati? Praesentium enim, ea quaerat consequatur voluptas
+        expedita ab impedit temporibus id, libero, odit voluptatem eveniet
+        tempore corporis quos. Quam, sint voluptatum? Cum doloribus culpa
+        blanditiis placeat excepturi fugiat nostrum similique quis expedita. A
+        excepturi rerum ut porro saepe, natus nostrum voluptatem aspernatur unde
+        minus hic, vero libero quasi molestias exercitationem eveniet quis
+        reprehenderit tempora veritatis voluptate cumque! Vitae ut commodi
+        voluptatibus asperiores, atque laborum similique officia ex quis hic
+        harum, quam ratione ducimus, accusamus eius illum quos eveniet? Sequi
+        repellendus ipsam in autem pariatur quam debitis quaerat facere culpa
+        dicta illo ducimus cupiditate reprehenderit nam veritatis perferendis
+        consequuntur, quasi perspiciatis dignissimos minus deleniti.
+        Consectetur, provident. Hic odit consectetur beatae accusantium,
+        exercitationem eligendi.
+      </p>
+    </article>
+  )
+}
+
+export default Tekst
