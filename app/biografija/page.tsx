00c18485c0518c47dea574cef7445bbc09b24a35
Pera Knezevic <predragknezevic@icloud.com>
2023-11-30 22:45:49 +0100

    Main page updated

--- app/biografija/page.tsx
+++ app/biografija/page.tsx
@@ -1,4 +1,5 @@
 import prisma from "@/prisma/client"
+import Image from "next/image"
 import React from "react"
 import ReactMarkdown from "react-markdown"
 
@@ -12,6 +13,17 @@ const Biografija = async () => {
     <article>
       <h1>Biografija</h1>
       <div className="prose lg:prose-xl mx-auto">
+        <figure>
+          <Image
+            src="/slike/marijana-canak.jpg"
+            alt="Marijana Čanak"
+            width={1600}
+            height={1100}
+          />
+          <figcaption>
+            <p>Marijana Čanak, autorka fotografije Maja Tomić</p>
+          </figcaption>
+        </figure>
         <ReactMarkdown>{biografija?.uvod}</ReactMarkdown>
         <ReactMarkdown>{biografija?.tekst}</ReactMarkdown>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,53 @@
 import prisma from "@/prisma/client"
+import Image from "next/image"
+import Link from "next/link"
 
 const Home = async () => {
-  const dobrodosli = await prisma.stranica.findUnique({
-    where: { slug: "dobro-dosli" },
+  const biografija = await prisma.stranica.findUnique({
+    where: { slug: "biografija" },
   })
-  if (!dobrodosli) return <h1>Dobro došli</h1>
   return (
-    <div className="my-10">
-      <h1>{dobrodosli.naslov}</h1>
-      <p>{dobrodosli.uvod}</p>
+    <div className="grid grid-cols-2 grid-rows-2 max-w-5xl mx-auto my-16 gap-16 text-lg">
+      <div>
+        {biografija?.uvod}
+        <p className="text-right">
+          <Link href="/biografija" className="btn btn-sm ">
+            Kompletna biografija
+          </Link>
+        </p>
+      </div>
+      <div>
+        <figure>
+          <Link href="/biografija">
+            <Image
+              src="/slike/marijana-canak.jpg"
+              alt="Marijana Čanak"
+              width={1600}
+              height={1100}
+              className="rounded-2xl"
+            />
+          </Link>
+          <figcaption>
+            <p>Marijana Čanak, fotografija: Maja Tomić</p>
+          </figcaption>
+        </figure>
+      </div>
+      <div>
+        <figure>
+          <Link href="/knjige">
+            <Image
+              src="/slike/marijana-canak-knjige.jpg"
+              alt="Knjige Marijane Čanak"
+              width={1600}
+              height={1100}
+              className="rounded-2xl"
+            />
+          </Link>
+          <figcaption>
+            <p>knjige Marijane Čanak, fotografija: Maja Tomić</p>
+          </figcaption>
+        </figure>
+      </div>
     </div>
   )
 }
