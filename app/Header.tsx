72d7d853aca6faa49c548e723c0d1e2202bf241f
Pera Knezevic <predragknezevic@icloud.com>
2023-12-12 08:54:24 +0100

    Social added updated

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import Link from "next/link"
-import { FaGoodreads } from "react-icons/fa"
+import { FaGoodreads, FaInstagramSquare, FaPatreon } from "react-icons/fa"
 import { Bodoni_Moda } from "next/font/google"
 
 const bodoni = Bodoni_Moda({
@@ -40,10 +40,27 @@ const Header = () => {
           <li>
             <Link href="/kontakt">Kontakt</Link>
           </li>
+
+          <li>
+            <a
+              href="httpshttps://www.patreon.com/MarijanaCanak"
+              title="Marijana na Patreonu"
+            >
+              <FaPatreon className="-mb-1" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/canakmarijana/"
+              title="Marijana na Instagramu"
+            >
+              <FaInstagramSquare className="-mb-1" />
+            </a>
+          </li>
           <li>
             <a
               href="https://www.goodreads.com/author/show/8286470.Marijana_anak"
-              title="Marijana on GoodReads"
+              title="Marijana na GoodReads"
             >
               <FaGoodreads className="-mb-1" />
             </a>
